ea42ff6bb583e2c59155d125c25b5e6c49e2b6d3
Sandeep Raghuraman <sandy.8925@gmail.com>
2013-12-27 19:02:19 -0800

    Create new activity - SettingsActivity

---
+++ src/org/sanpra/kiki/activity/SettingsActivity.java
@@ -0,0 +1,13 @@
+package org.sanpra.kiki.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+    }
+
+}
