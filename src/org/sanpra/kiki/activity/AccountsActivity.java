a59b8e448017ea0f7ca2fba481edeb9341be0167
Sandeep Raghuraman <sandy.8925@gmail.com>
2013-12-30 13:17:47 -0800

    Created AccountsActivity class and corresponding layout

---
+++ src/org/sanpra/kiki/activity/AccountsActivity.java
@@ -0,0 +1,17 @@
+package org.sanpra.kiki.activity;
+
+import org.sanpra.kiki.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AccountsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+
+	setContentView(R.layout.account_list_layout);
+    }
+
+}
