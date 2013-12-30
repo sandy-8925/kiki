f9a8599c088c64d94384833bd931d2a4e6cbe3a2
Sandeep Raghuraman <sandy.8925@gmail.com>
2013-12-30 13:29:55 -0800

    Launch Accounts activity on clicking the Accounts button in the Settings activity

--- src/org/sanpra/kiki/activity/SettingsActivity.java
+++ src/org/sanpra/kiki/activity/SettingsActivity.java
@@ -3,7 +3,10 @@ package org.sanpra.kiki.activity;
 import org.sanpra.kiki.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends Activity {
 
@@ -12,6 +15,15 @@ public class SettingsActivity extends Activity {
 	super.onCreate(savedInstanceState);
 
 	setContentView(R.layout.settings_form);
+
+	Button accountsButton = (Button) findViewById(R.id.settings_form_accounts_button);
+	accountsButton.setOnClickListener(new View.OnClickListener() {
+	    @Override
+	    public void onClick(View clickedView) {
+		startActivity(new Intent(clickedView.getContext(),
+			AccountsActivity.class));
+	    }
+	});
     }
 
 }
