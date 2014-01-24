7babfc716dd5ba71001bbe5980fc0f8506921e42
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-01-24 14:28:05 -0800

    NewAccountTypeSelectActivity: Create new ArrayAdapter object in it's own statement, and a reference to refer to it. Use Android's existing simple layout resource

--- src/org/sanpra/kiki/activity/NewAccountTypeSelectActivity.java
+++ src/org/sanpra/kiki/activity/NewAccountTypeSelectActivity.java
@@ -1,18 +1,19 @@
 package org.sanpra.kiki.activity;
 
-import org.sanpra.kiki.R;
 import org.sanpra.kiki.account.CommonAccountInfo;
 
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 
 public class NewAccountTypeSelectActivity extends ListActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);		
-		setListAdapter(new ArrayAdapter<String>(this, R.id.accountTypeListText, CommonAccountInfo.availableAccountTypes));
+		super.onCreate(savedInstanceState);
+		ListAdapter accountTypeListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, CommonAccountInfo.availableAccountTypes);
+		setListAdapter(accountTypeListAdapter);
 	}
 
 }
