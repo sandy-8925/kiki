88c6f2072e53b9ce77ea1e6127c6df1dc60fa037
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-05-29 20:39:59 -0700

    Account: Make accountType protected

--- src/org/sanpra/kiki/account/Account.java
+++ src/org/sanpra/kiki/account/Account.java
@@ -4,5 +4,5 @@ package org.sanpra.kiki.account;
  * Represents service account (Facebook, Google+, Flickr etc.) of any type
  */
 abstract class Account {
-    private AccountType accountType;
+    protected AccountType accountType;
 }
