f916f4977b964bd57824be20ab176fe7fbe6106d
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-06-03 19:07:36 -0700

    AccountType: Improve javadoc

--- src/org/sanpra/kiki/account/AccountType.java
+++ src/org/sanpra/kiki/account/AccountType.java
@@ -2,7 +2,8 @@ package org.sanpra.kiki.account;
 
 /**
  * Represents an account type (e.g) Facebook account, Google account etc.
- * Each AccountType implementation must have a logo, service name
+ * Each AccountType implementation must have a logo, service name, and a
+ * method to create an account of that type.
  */
 public interface AccountType {
     public Account createAccount();
