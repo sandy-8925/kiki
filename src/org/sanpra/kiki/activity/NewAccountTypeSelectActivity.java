abd1af25c0d40b0da25e2b7254d9f351013e7305
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-07-11 18:16:10 -0700

    Modify NewAccountTypeSelectActivity to use AccountTypeListAdapter

--- src/org/sanpra/kiki/activity/NewAccountTypeSelectActivity.java
+++ src/org/sanpra/kiki/activity/NewAccountTypeSelectActivity.java
@@ -3,13 +3,9 @@ package org.sanpra.kiki.activity;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import org.sanpra.kiki.account.AccountManager;
-import org.sanpra.kiki.account.AccountType;
-
-import java.util.List;
 
 /**
  * Shows list of account types. User selects one of the account types to create a new account of that type.
@@ -19,9 +15,7 @@ public final class NewAccountTypeSelectActivity extends ListActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-        List<AccountType> accountList =  AccountManager.getAccountTypeList();
-        //TODO:use cursor/adapter to display account list
-		ListAdapter accountTypeListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, AccountManager.availableAccountTypes);
+		ListAdapter accountTypeListAdapter = new AccountTypeListAdapter(AccountManager.getAvailableAccountTypes());
 		setListAdapter(accountTypeListAdapter);
 	}
 
