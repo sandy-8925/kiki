d421aaedf28c2e73a13ae55baac99a2d2578b3e1
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-07-22 16:31:36 -0700

    AccountsActivity: Use AccountListAdapter as adapter for the accounts_list element

--- src/org/sanpra/kiki/activity/AccountsActivity.java
+++ src/org/sanpra/kiki/activity/AccountsActivity.java
@@ -1,10 +1,11 @@
 package org.sanpra.kiki.activity;
 
-import org.sanpra.kiki.R;
-import org.sanpra.kiki.utils.StartActivityOnClickListener;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
+import org.sanpra.kiki.R;
+import org.sanpra.kiki.account.AccountManager;
+import org.sanpra.kiki.utils.StartActivityOnClickListener;
 
 public final class AccountsActivity extends Activity {
 
@@ -15,7 +16,9 @@ public final class AccountsActivity extends Activity {
         setContentView(R.layout.account_list_layout);
         findViewById(R.id.addAccountButton).setOnClickListener(new StartActivityOnClickListener(NewAccountTypeSelectActivity.class));
 
-
+        //TODO: Need to dynamically refresh this data when new accounts are created
+        final AccountListAdapter accountListAdapter = new AccountListAdapter(AccountManager.getAccountList());
+        ((ListView) findViewById(R.id.accounts_list)).setAdapter(accountListAdapter);
     }
 
 }
