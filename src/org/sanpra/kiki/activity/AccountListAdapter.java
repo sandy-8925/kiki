627e6952d1991b58667874aa4f017b66d03f5910
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-07-22 16:30:40 -0700

    Create new class AccountListAdapter - ListAdapter implementation used for displaying list of accounts

---
+++ src/org/sanpra/kiki/activity/AccountListAdapter.java
@@ -0,0 +1,90 @@
+package org.sanpra.kiki.activity;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+import org.sanpra.kiki.account.Account;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+final class AccountListAdapter implements ListAdapter {
+
+    private List<Account> accountList;
+
+    AccountListAdapter(Collection<Account> accountList) {
+        this.accountList = new ArrayList<Account>();
+        this.accountList.addAll(accountList);
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return true;
+    }
+
+    @Override
+    public void registerDataSetObserver(android.database.DataSetObserver observer) {
+
+    }
+
+    @Override
+    public void unregisterDataSetObserver(android.database.DataSetObserver observer) {
+
+    }
+
+    @Override
+    public int getCount() {
+        return accountList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return accountList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater layoutInflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View listItem = layoutInflater.inflate(android.R.layout.simple_list_item_2, parent, false);
+
+        final Account relatedAccount = accountList.get(position);
+        ((TextView) listItem.findViewById(android.R.id.text1)).setText(relatedAccount.getDisplayName());
+        ((TextView) listItem.findViewById(android.R.id.text2)).setText(relatedAccount.getAccountType().getServiceName());
+
+        return listItem;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return IGNORE_ITEM_VIEW_TYPE;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return accountList.isEmpty();
+    }
+}
