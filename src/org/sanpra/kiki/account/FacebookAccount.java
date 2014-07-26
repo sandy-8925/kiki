2171f36835d5187da0878e8d766b397b9553e8ec
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-07-25 20:13:12 -0700

    FacebookAccount: Add TODO in FacebookAccount - check for invalid input in constructor

--- src/org/sanpra/kiki/account/FacebookAccount.java
+++ src/org/sanpra/kiki/account/FacebookAccount.java
@@ -8,6 +8,7 @@ import com.facebook.Session;
 final class FacebookAccount extends Account {
     private Session fbSession;
 
+    //TODO: Test for null input and throw exception. Or check in FacebookAccountType.createAccount()
     FacebookAccount(Session session) {
         super(FacebookAccountType.getInstance());
         this.fbSession = session;
